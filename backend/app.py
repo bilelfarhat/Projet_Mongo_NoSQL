4ae209dea1f4505ee5a5d41e1e811a181f0929ef
bilelfarhat <124139134+bilelfarhat@users.noreply.github.com>
2024-12-05 20:14:06 +0000

    last commit

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})  # Configurer CORS pour permettre les interactions avec le frontend
 
 # Connexion à MongoDB
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb://mongo:27017/")
 db = client["library-db"]
 
 # Collections MongoDB
